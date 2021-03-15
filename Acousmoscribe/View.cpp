55c61e1e7d6160d08fb63ebd490f0ce5125b87f2
Loïc Lachiver <loic.lachiver@gmail.com>
2021-03-15 13:40:39 +0100

    deleted useless code in View.cpp

--- Acousmoscribe/View.cpp
+++ Acousmoscribe/View.cpp
@@ -37,64 +37,6 @@ bool View::canEdit() const
 
 void View::paint_impl(QPainter* p) const
 {
-  if (auto v = getView(*this))
-  {
-    if (canEdit())
-    {
-      const double dpi = p->device()->devicePixelRatioF();
-      const auto view_left = v->mapToScene(0, 0);
-      const auto left = std::max(0., this->mapFromScene(view_left).x());
-      double x = left;
-      const double text_w = 30. * dpi;
-      const double next_w = m_bgCache.width() - text_w;
-      const double proc_w = width();
-      const double h = std::round(m_bgCache.height() / 2.);
-
-      if (p->device()->devicePixelRatioF() == 1.)
-      {
-        m_fragmentCache.clear();
-        while(x - next_w < proc_w)
-        {
-          m_fragmentCache.push_back(QPainter::PixmapFragment{
-              x,
-              h,
-              text_w,
-              0.,
-              next_w,
-              (double)m_bgCache.height(),
-              1.,
-              1.,
-              0.,
-              1.});
-          x += next_w;
-        }
-
-        p->drawPixmapFragments(
-            m_fragmentCache.data(),
-            m_fragmentCache.size(),
-            m_bgCache);
-      }
-      else
-      {
-        while(x - next_w < proc_w)
-        {
-          p->drawPixmap(QPointF{x, 0}, m_bgCache, QRectF{text_w, 0., next_w, (double)m_bgCache.height()});
-          x += next_w / dpi;
-        }
-      }
-
-      if(left < 30 && !m_textCache.isNull())
-      {
-        p->drawPixmap(QPointF{}, m_textCache);
-      }
-    }
-  }
-  if (!m_selectArea.isEmpty())
-  {
-    /*p->setBrush(style.transparentBrush);
-    p->setPen(style.selectionPen);*/
-    p->drawPath(m_selectArea);
-  }
   p->drawRect(30, 50, 30, 50);
 
   /* rectangle/triangle */
