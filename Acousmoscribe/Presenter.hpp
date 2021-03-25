2404ee26c870d94767ce9115a063db9196d7676c
theurtel <thomas.heurtel@outlook.fr>
2021-03-25 11:48:10 +0100

    ajout des commandes dans Presenter.hpp (à modifier)

--- Acousmoscribe/Presenter.hpp
+++ Acousmoscribe/Presenter.hpp
@@ -26,7 +26,44 @@ public:
 
   void parentGeometryChanged() override;
 
+  //void on_spectralKeyChanged(SpectralKeyView& sKey);
+  void on_spectralKeyNature1Changed(Nature& nature1);
+  void on_spectralKeyNature2Changed(Nature& nature2);
+  void on_spectralKeyIsRichChanged(bool& isRich);
+  void on_spectralKeyIsHybridChanged(bool& isHybrid);
+
+  //void on_melodicKeyChanged(MelodicKeyView& mKey);
+  void on_melodicKeyPitchChanged(Pitch& pitch);
+  void on_melodicKeyRangeChanged(Range& range);
+
+  void on_deselectOtherSigns();
+  void on_signDuplicate();
+  void on_signScaled(const Sign&, double newScale);
+  //void on_signChanged(SignView& sign);
+  void on_signAttackChanged(Dyn& attack);
+  void on_signAttackChangeFinished(Dyn& attack);
+  void on_signReleaseChanged(Dyn& release);
+  void on_signReleaseChangeFinished(Dyn& release);
+  //void on_signDynamicProfileChanged(DynamicProfile& dynP);
+  void on_signMelodicProfileChanged(MelodicProfile& melP);
+  void on_signRhythmicProfileChanged(RhythmicProfile& rhyP);
+  void on_signGrainChanged(Grain& grain);
+
+  /*void on_signvolumeStartChanged(volume ?);
+  void on_signvolumeStartChangeFinished(volume ?);
+  void on_signvolumeEndChanged(volume ?);
+  void on_signvolumeEndChangeFinished(volume ?);*/
+
 private:
+  void on_spectralKeyAdded(const SpectralKey&);
+  void on_spectralKeyRemoving(const SpectralKey&);
+
+  void on_melodicKeyAdded(const MelodicKey&);
+  void on_melodicKeyRemoving(const MelodicKey&);
+
+  void on_SignAdded(const Sign&);
+  void on_SignRemoving(const Sign&);
+
   const Model& m_model;
   View* m_view{};
 };
