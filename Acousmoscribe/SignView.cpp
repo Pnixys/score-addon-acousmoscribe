28cd03246097dda2ccd26da4e0be3fb72b1929c8
adeynoux <antoine.deynoux@etu.u-bordeaux.fr>
2021-03-26 14:47:48 +0100

    SignView

--- Acousmoscribe/SignView.cpp
+++ Acousmoscribe/SignView.cpp
@@ -1,11 +1,21 @@
 #include "SignView.hpp"
 
+#include <Acousmoscribe/View.hpp>
+#include <Acousmoscribe/Presenter.hpp>
+
+#include <QCursor>
+#include <QGraphicsSceneMouseEvent>
+#include <QGuiApplication>
+#include <QPainter>
 
 namespace Acousmoscribe
 {
 
 SignView::SignView(QGraphicsItem* parent)
     : QGraphicsItem{parent}
+    , sign{n}
+    , m_presenter{p}
+    , m_action{None}
 {
     this->setFlag(QGraphicsItem::ItemIsSelectable, true);
     this->setFlag(QGraphicsItem::ItemIsMovable, false);
@@ -13,7 +23,213 @@ SignView::SignView(QGraphicsItem* parent)
     this->setAcceptHoverEvents(true);
 }
 
-void SignView::paint(QPainter* p) const
+/*void SignView::paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget)
 {
+  static const MidiStyle s;
+
+  painter->setRenderHint(QPainter::Antialiasing, false);
+
+  if (m_width <= 1.2)
+  {
+    painter->setPen(this->isSelected() ? s.signSelectedBasePen : s.signBasePen);
+    painter->drawLine(0, 0, 0, m_height - 1.5);
+  }
+  else
+  {
+    painter->setPen(this->isSelected() ? s.signSelectedBasePen : Qt::NoPen);
+    painter->setBrush(s.paintedSignBrush[sign.velocity()]);
+
+    painter->drawRect(boundingRect().adjusted(0., 0., 0., 0));
+  }
+}*/
+
+QPointF SignView::closestPos(QPointF newPos) const noexcept
+{
+  auto& view = *(View*)parentItem();
+  const auto [min, max] = view.range();
+  const double count = view.visibleCount();
+  const QRectF rect = view.boundingRect();
+  const auto height = rect.height();
+  const auto sign_height = height / count;
+
+  newPos.setX(qMin(rect.right(), qMax(newPos.x(), rect.left())));
+  auto bounded = (qBound(rect.top(), newPos.y() - sign_height * 0.45, rect.bottom())) / height;
+  int sign = qBound(min, int(max - bounded * count), max);
+
+  newPos.setY(height - (sign - min + 1) * sign_height);
+  return newPos;
+}
+
+QRectF SignView::computeRect() const noexcept
+{
+  auto& view = *(View*)parentItem();
+  const auto h = view.height();
+  const auto w = view.defaultWidth();
+  const auto [min, max] = view.range();
+  const auto sign_height = h / view.visibleCount();
+  const QRectF rect{
+      sign.start() * w,
+      h - std::ceil((sign.pitch() - min + 1) * sign_height),
+      sign.duration() * w,
+      sign_height};
+
+  return rect;
+}
+
+QVariant SignView::itemChange(QGraphicsItem::GraphicsItemChange change, const QVariant& value)
+{
+  switch (change)
+  {
+    case QGraphicsItem::ItemSelectedChange:
+    {
+      this->setZValue(10 * (int)value.toBool());
+      break;
+    }
+    default:
+      break;
+  }
+
+  return QGraphicsItem::itemChange(change, value);
+}
+
+void SignView::hoverEnterEvent(QGraphicsSceneHoverEvent* event)
+{
+  if (event->pos().x() >= this->boundingRect().width() - 2)
+  {
+    auto& skin = score::Skin::instance();
+    this->setCursor(skin.CursorScaleH);
+  }
+  else
+  {
+    this->setCursor(Qt::ArrowCursor);
+  }
+
+  QGraphicsItem::hoverEnterEvent(event);
+}
+
+void SignView::hoverMoveEvent(QGraphicsSceneHoverEvent* event)
+{
+  if (event->pos().x() >= this->boundingRect().width() - 2)
+  {
+    auto& skin = score::Skin::instance();
+    this->setCursor(skin.CursorScaleH);
+  }
+  else
+  {
+    if(qApp->keyboardModifiers() == Qt::ShiftModifier)
+    {
+      auto& skin = score::Skin::instance();
+      this->setCursor(skin.CursorSpin);
+    }
+    else
+    {
+      this->setCursor(Qt::ArrowCursor);
+    }
+  }
+
+  QGraphicsItem::hoverMoveEvent(event);
+}
+
+void SignView::hoverLeaveEvent(QGraphicsSceneHoverEvent* event)
+{
+  this->setCursor(Qt::ArrowCursor);
+
+  QGraphicsItem::hoverEnterEvent(event);
+}
+
+bool SignView::canEdit() const
+{
+  return boundingRect().height() > 5;
+}
+
+static QPointF signview_origpoint;
+void SignView::mousePressEvent(QGraphicsSceneMouseEvent* event)
+{
+  const auto mods = QGuiApplication::keyboardModifiers();
+  if (!(mods & Qt::ControlModifier) && !isSelected())
+    m_presenter.on_deselectOtherSigns();
+
+  setSelected(true);
+
+  m_action = None;
+  if (canEdit())
+  {
+    if (event->pos().x() >= this->boundingRect().width() - 2)
+    {
+      m_action = Scale;
+    }
+    else if (mods & Qt::ShiftModifier)
+    {
+      m_action = ChangeVelocity;
+    }
+    else if (mods & Qt::AltModifier)
+    {
+      m_action = Duplicate;
+    }
+    else
+    {
+      m_action = Move;
+      signview_origpoint = this->pos();
+    }
+  }
+  event->accept();
+}
+
+void SignView::mouseMoveEvent(QGraphicsSceneMouseEvent* event)
+{
+  if (canEdit())
+  {
+    switch(m_action)
+    {
+      case Move:
+        this->setPos(closestPos(
+            signview_origpoint + event->scenePos() - event->buttonDownScenePos(Qt::LeftButton)));
+         m_presenter.on_signChanged(*this);
+        break;
+      case Scale:
+        this->setWidth(std::max(2., event->pos().x()));
+        break;
+      case Duplicate:
+        m_presenter.on_duplicate();
+        break;
+      case ChangeVelocity:
+        m_presenter.on_requestVelocityChange(sign, event->buttonDownScenePos(Qt::LeftButton).y() - event->scenePos().y());
+        break;
+      case None:
+        break;
+    }
+  }
+  event->accept();
+}
+
+void SignView::mouseReleaseEvent(QGraphicsSceneMouseEvent* event)
+{
+  if (canEdit())
+  {
+    switch(m_action)
+    {
+      case Move:
+        this->setPos(closestPos(
+            signview_origpoint + event->scenePos() - event->buttonDownScenePos(Qt::LeftButton)));
+         m_presenter.on_signChanged(*this);
+         m_presenter.on_signChangeFinished(*this);
+        break;
+      case Scale:
+        this->setWidth(std::max(2., event->pos().x()));
+        m_presenter.on_signScaled(sign, m_width / ((View*)parentItem())->defaultWidth());
+        break;
+      case Duplicate:
+        m_presenter.on_duplicate();
+        break;
+      case ChangeVelocity:
+        m_presenter.on_requestVelocityChange(sign, event->buttonDownScenePos(Qt::LeftButton).y() - event->scenePos().y());
+        m_presenter.on_velocityChangeFinished();
+        break;
+      case None:
+        break;
+    }
+  }
+  event->accept();
+  m_action = None;
 }
 }
--- Acousmoscribe/SignView.hpp
+++ Acousmoscribe/SignView.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <Acousmoscribe/View.hpp>
+
 #include <QGraphicsItem>
 
 namespace Acousmoscribe
@@ -16,10 +17,54 @@ struct Sign
 {
   Sign() = default;
 };
-  SignView(QGraphicsItem* parent);
+  SignView(const Note& n, Presenter& presenter, QGraphicsItem* parent);
   Sign Sign() const noexcept;
 
+  void setWidth(qreal w) noexcept
+  {
+    if (m_width != w)
+    {
+      prepareGeometryChange();
+      m_width = w;
+    }
+  }
+
+  void setHeight(qreal h) noexcept
+  {
+    if (m_height != h)
+    {
+      prepareGeometryChange();
+      m_height = h;
+    }
+  }
+
+  QRectF boundingRect() const override { return {0, 0, m_width, m_height}; }
+  void paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget) override;
+
+  QRectF computeRect() const noexcept;
+  QPointF closestPos(QPointF note) const noexcept;
+
 private:
-  void paint(QPainter*) const;
+  bool canEdit() const;
+  QVariant itemChange(GraphicsItemChange change, const QVariant& value) override;
+  void hoverEnterEvent(QGraphicsSceneHoverEvent* event) override;
+  void hoverMoveEvent(QGraphicsSceneHoverEvent* event) override;
+  void hoverLeaveEvent(QGraphicsSceneHoverEvent* event) override;
+  void mousePressEvent(QGraphicsSceneMouseEvent* event) override;
+  void mouseMoveEvent(QGraphicsSceneMouseEvent* event) override;
+  void mouseReleaseEvent(QGraphicsSceneMouseEvent* event) override;
+
+  Presenter& m_presenter;
+
+  float m_width{};
+  float m_height{};
+
+  enum Action {
+    None,
+    Move,
+    Scale,
+    ChangeVelocity,
+    Duplicate
+  } m_action{};
 };
 }
