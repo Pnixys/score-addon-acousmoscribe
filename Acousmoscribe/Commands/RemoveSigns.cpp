8d825b327763e9ff7fc9d06289d1195212b10ab8
Julien Daugaron <julien.daugaron@etu.u-bordeaux.fr>
2021-04-01 16:35:29 +0200

    removes Signs done ça compile

---
+++ Acousmoscribe/Commands/RemoveSigns.cpp
@@ -0,0 +1,50 @@
+#include "RemoveSigns.hpp"
+
+#include <Acousmoscribe/Process.hpp>
+#include <Process/TimeValueSerialization.hpp>
+
+#include <score/model/path/PathSerialization.hpp>
+#include <score/tools/IdentifierGeneration.hpp>
+
+namespace Acousmoscribe{
+
+
+RemoveSigns::RemoveSigns(const Model& model, const std::vector<Id<Sign>>& signs)
+    : m_model{model}
+{
+  for (auto id : signs)
+  {
+    auto& sign = model.signs.at(id);
+    m_signs.push_back(
+        qMakePair(id, SignData{sign.start(), sign.duration(), sign.grain(), sign.dynamicProfile(), sign.melodicProfile(), sign.rhythmicProfile()}));
+  }
+}
+
+void RemoveSigns::undo(const score::DocumentContext& ctx) const
+{
+  auto& model = m_model.find(ctx);
+  for (auto& sign : m_signs)
+  {
+    model.signs.add(new Sign{sign.first, sign.second, &model});
+  }
+}
+
+void RemoveSigns::redo(const score::DocumentContext& ctx) const
+{
+  auto& model = m_model.find(ctx);
+  for (auto& sign : m_signs)
+  {
+    model.signs.remove(sign.first);
+  }
+}
+
+void RemoveSigns::serializeImpl(DataStreamInput& s) const
+{
+  s << m_model << m_signs;
+}
+
+void RemoveSigns::deserializeImpl(DataStreamOutput& s)
+{
+  s >> m_model >> m_signs;
+}
+}
