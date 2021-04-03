bdfe17ba2d96d0554e504b1c2d5cced81abdb2ea
Loïc Lachiver <loic.lachiver@gmail.com>
2021-04-03 16:27:45 +0200

    replaceSigns was useless

--- Acousmoscribe/Commands/AddSign.hpp
+++ Acousmoscribe/Commands/AddSign.hpp
@@ -29,30 +29,4 @@ private:
   Id<Sign> m_id;
   SignData m_sign;
 };
-
-class ReplaceSigns final : public score::Command
-{
-  SCORE_COMMAND_DECL(Acousmoscribe::CommandFactoryName(), ReplaceSigns, "Set signs")
-public:
-  ReplaceSigns(
-      const Model& model,
-      const std::vector<SignData>& sign,
-      int min,
-      int max,
-      TimeVal dur);
-
-  void undo(const score::DocumentContext& ctx) const override;
-  void redo(const score::DocumentContext& ctx) const override;
-
-
-protected:
-  void serializeImpl(DataStreamInput& s) const override;
-  void deserializeImpl(DataStreamOutput& s) override;
-
-
-private:
-  Path<Model> m_model;
-  std::vector<std::pair<Id<Sign>, SignData>> m_old, m_new;
-  TimeVal m_olddur{}, m_newdur{};
-};
 }
